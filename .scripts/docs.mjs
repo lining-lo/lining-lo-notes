cbebdb5d9cb724c1f7fa65ef8044ddfafe2a36d8
lining-lo <2092576148@qq.com>
2026-09-11 11:10:03 +0800

    2026/09/11 周五

--- .scripts/docs.mjs
+++ .scripts/docs.mjs
@@ -90,7 +90,8 @@ export const docs = {
     "Java/01_环境和语法",
     "Java/02_运算符与方法",
     "Java/03_流程控制语句",
-    "Java/04_数组的使用"
+    "Java/04_数组的使用",
+    "Java/05_面相对象编程"
   ],
   "JavaScript": [
     "JavaScript/Ajax请求",
